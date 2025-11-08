5af5f210174fad6250406dd0ef4823825bed7705
ELMEHDI ACHAHED <117928790+Trpsky@users.noreply.github.com>
2025-11-08 09:33:16 +0100

    feat : implement new demo data into cars table

--- 1-Cars/script.js
+++ 1-Cars/script.js
@@ -5,3 +5,15 @@ function Car(model , brand , year , type , fuel){
     this.type = type;
     this.fuel = fuel;
 }
+
+t = [];
+
+let car1 = new Car("Model 3", "Tesla", 2024, "Sedan", "Electric");
+let car2 = new Car("F-150", "Ford", 2023, "Truck", "Gasoline");
+let car3 = new Car("911 Carrera", "Porsche", 2022, "Sports Car", "Gasoline");
+let car4 = new Car("CR-V", "Honda", 2025, "SUV", "Hybrid");
+let car5 = new Car("Golf", "Volkswagen", 2021, "Hatchback", "Diesel");
+
+t.push(car1 , car2 , car3 , car4 , car5);
+
+console.log(t);
