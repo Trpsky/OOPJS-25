4b1164e7fbc23dca6394e9da58f358a33a0082fd
ELMEHDI ACHAHED <117928790+Trpsky@users.noreply.github.com>
2025-11-09 15:01:46 +0100

    feat : implement segement testing in main script

--- 3-Vector/script.js
+++ 3-Vector/script.js
@@ -1,6 +1,8 @@
 import Rectangle from "./Rectangle.js";
 import Square from "./Square.js";
 import Point from "./Point.js";
+import Segement from "./Segement.js";
+
 let r = new Rectangle(3, 5);
 let s = new Square(3, 3);
 console.log(s.show());
@@ -12,3 +14,7 @@ console.log(r.surface());
 let p = new Point(1, 2);
 let p1 = new Point(3, 4);
 console.log(p , p1);
+console.log("-----");
+
+let seg = new Segement(1,2,3,4);
+console.log(seg);
