55b4e8499db24eff0ecef4e0cbe88daf6ceaf108
ELMEHDI ACHAHED <117928790+Trpsky@users.noreply.github.com>
2025-11-09 15:00:14 +0100

    fix : fix point import issue

--- 3-Vector/script.js
+++ 3-Vector/script.js
@@ -1,6 +1,6 @@
 import Rectangle from "./Rectangle.js";
 import Square from "./Square.js";
-
+import Point from "./Point.js";
 let r = new Rectangle(3, 5);
 let s = new Square(3, 3);
 console.log(s.show());
@@ -8,3 +8,7 @@ console.log(s.surface());
 console.log("-----");
 console.log(r.show());
 console.log(r.surface());
+
+let p = new Point(1, 2);
+let p1 = new Point(3, 4);
+console.log(p , p1);
