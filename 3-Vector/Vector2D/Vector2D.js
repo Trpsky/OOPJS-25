ee0c60d5f8cac0358136dda927082f888af96650
ELMEHDI ACHAHED <117928790+Trpsky@users.noreply.github.com>
2025-11-09 15:07:35 +0100

    chore(structure): reorganize files into folders based on their case and move all test cases to main script

--- 3-Vector/Vector2D/Vector2D.js
+++ 3-Vector/Vector2D/Vector2D.js
@@ -18,15 +18,6 @@ class Vector2D {
     }
 }
 
-let v1 = new Vector2D();
-let v2 = new Vector2D(1 , 9);
-let v3 = new Vector2D(1);
-let v4 = new Vector2D(undefined,1);
+export default Vector2D;
 
-console.log(v1.show()); 
-console.log(v2.show()); 
-console.log(v1.addition(v2)); 
-console.log(v2); 
-console.log(v3); 
-console.log(v4); 
 
--- 3-Vector/script.js
+++ 3-Vector/script.js
@@ -1,7 +1,8 @@
-import Rectangle from "./Rectangle.js";
-import Square from "./Square.js";
-import Point from "./Point.js";
-import Segement from "./Segement.js";
+import Rectangle from "./Shapes/Rectangle.js";
+import Square from "./Shapes/Square.js";
+import Point from "./Point/Point.js";
+import Segement from "./Point/Segement.js";
+import Vector2D from "./Vector2D/Vector2D.js";
 
 let r = new Rectangle(3, 5);
 let s = new Square(3, 3);
@@ -18,3 +19,17 @@ console.log("-----");
 
 let seg = new Segement(1,2,3,4);
 console.log(seg.show());
+
+console.log("-----");
+
+let v1 = new Vector2D();
+let v2 = new Vector2D(1 , 9);
+let v3 = new Vector2D(1);
+let v4 = new Vector2D(undefined,1);
+
+console.log(v1.show()); 
+console.log(v2.show()); 
+console.log(v1.addition(v2)); 
+console.log(v2); 
+console.log(v3); 
+console.log(v4); 
