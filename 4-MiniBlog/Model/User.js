3f001d2c7ddf8ebbc7f4ba213d939942570fd1b0
ELMEHDI ACHAHED <117928790+Trpsky@users.noreply.github.com>
2025-11-09 15:49:53 +0100

    feat (MiniBlog) : implement User methods with JSON storage
    - Added constructor to initialize a new user and automatically assign a unique id
    - Added index() method to return list of users
    - Added store() method to save new users into users.json
    - Integrated file system and path modules to handle JSON storage

--- 4-MiniBlog/Model/User.js
+++ 4-MiniBlog/Model/User.js
@@ -1,12 +1,46 @@
-class User {
-    firstName;
-    lastName;  
+import users from '../database/user.json' with  { type: 'json' };
+
+import { writeFileSync } from 'fs';
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const usersPath = resolve(__dirname, '../database/user.json');
+
+export default class User {
+    id;
+    first_name;
+    last_name;
     email;
     password;
-    constructor(firstName, lastName, email, password) {
-        this.firstName = firstName;
-        this.lastName = lastName;
+    constructor(first_name, last_name, email, password) {
+        this.id = users.length + 1;
+        this.first_name = first_name;
+        this.last_name = last_name;
         this.email = email;
         this.password = password;
+        console.log("User created:", this.first_name, this.last_name);
     }
+
+    index() {
+        return users.length;
+    }
+
+    store() {
+        let newUser = {
+            "id": this.id,
+            "first_name": this.first_name,
+            "last_name": this.last_name,
+            "email": this.email,
+            "password": this.password
+        };
+        users.push(newUser);
+
+        writeFileSync(usersPath, JSON.stringify(users, null, 2));
+
+        return newUser;
+        // console.log("User stored:", newUser);
+    }
+
 }
