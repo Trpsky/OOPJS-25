4658c4a240eb99c0d44e2f1ccccfb36d14d58118
ELMEHDI ACHAHED <117928790+Trpsky@users.noreply.github.com>
2025-11-09 14:59:13 +0100

    feat : create Segementclass with constructor

---
+++ 3-Vector/Segement.js
@@ -0,0 +1,8 @@
+class Segement{
+    constructor(x_origin = 0 , y_origin = 0 , x_exterm = 0 , y_exterm = 0){
+        this.origin = new Point(x_origin , y_origin);
+        this.exterm = new Point(x_exterm , y_exterm);
+    }
+}
+
+export default Segement;
