b686691761ad0aba4dfb9db6db1c29dc275c8ac0
ELMEHDI ACHAHED <117928790+Trpsky@users.noreply.github.com>
2025-11-11 22:43:34 +0100

    Update PostController.js

--- 4-MiniBlog/Controller/PostController.js
+++ 4-MiniBlog/Controller/PostController.js
@@ -1,8 +1,16 @@
 import Post from '../Model/Post.js';
+import posts from '../database/post.json' with  { type: 'json' };
+
 export default class PostController {
     constructor(formElement, e) {
         this.formElement = formElement;
         this.event = e;
+    }
+
+    static async loadJSONToLocalStorage() {
+        const res = await fetch(posts);
+        const json = await res.json();
+        localStorage.setItem('posts', JSON.stringify(json));
     }
 
     initialize() {
@@ -16,15 +24,24 @@ export default class PostController {
         return formData;
     }
 
-    insertPost() {
+    insertPost(e) {
         let formData = this.initialize();
         let post = new Post(
-            formData.get('user_id'),
+            formData.get('user_id') || 1,
             formData.get('title'),
             formData.get('description')
         );
         let newPost = post.store();
         console.log('New Post Created:', newPost);
+        console.log(e);
+        this.form.reset();
         return newPost;
     }
+
+    static renderPosts(user_id) {
+        const post = new Post();
+        const posts = post.index(user_id);
+        console.log('Posts for User ID', user_id, ':', posts);
+        return posts;
+    }   
 }
