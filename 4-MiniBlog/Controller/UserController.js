75ba8268dafb6c53b5f5ef1e3131dab0c3afec96
ELMEHDI ACHAHED <117928790+Trpsky@users.noreply.github.com>
2025-11-13 21:39:50 +0100

    chore : add conditions to handle issue from model based on success true || false

--- 4-MiniBlog/Controller/UserController.js
+++ 4-MiniBlog/Controller/UserController.js
@@ -26,30 +26,56 @@ export default class UserController {
 
     initializeSignUp() {
         console.log("UserController Initialized in login");
-        this.event.preventDefault();
         const formData = new FormData(this.formElement);
         const firstName = formData.get('first_name');
         const lastName = formData.get('last_name');
         const email = formData.get('email');
         const password = formData.get('password');
-        console.log('Form Data:', { firstName , lastName, email, password });
+        console.log('Form Data:', { firstName, lastName, email, password });
         return formData;
     }
 
-    login() {
-        
+    static login(email, password) {
+        let response = User.login(email, password);
+        if (response.success) {
+            localStorage.setItem('user_id', response.user_id);
+            return response;
+        }
+        else {
+            localStorage.setItem('user_id', null);
+            return {
+                success: false,
+                "message": "Incorrect credentials"
+            }
+        }
     }
 
     signUp() {
         let formData = this.initializeSignUp();
+        if (User.checkIfEmailExists(formData.get("email"))) {
+            return {
+                "message" : "Email already exist !",
+                "success" : false
+            }
+        }
         let user = new User(
             formData.get('first_name'),
             formData.get('last_name'),
             formData.get('email'),
             formData.get('password'),
         );
-        let newUser = user.store();
-        console.log('New User Created:', newUser);
-        return true;
+        let response = user.store();
+        if (response.success) {
+            return {
+                "success": true,
+                "user": user,
+                "message": "Signup successful! 🎉\nYou will be redirected to login. Make sure to use the same credentials."
+            };
+        }
+        else {
+            return response;
+
+        }
+
     }
 }
